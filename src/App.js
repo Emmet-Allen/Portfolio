67294096bf7022d12da751dc1da4e26028aa3694
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-12-04 22:40:16 -0500

    Added Styles Folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import Projects from "./Projects";
-import About from "./About";
-import Articles from "./Articles";
+import Projects from "./Components/Projects";
+import About from "./Components/About";
+import Articles from "./Components/Articles";
 
 function App() {
   return (
--- src/Motion.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {motion} from "framer-motion";
-
-export const MyComponent = () => (
-    <motion.button
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-    />
-  )
